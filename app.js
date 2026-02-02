1e7320f63194ced4e7b7c02d361c717ef95b5f83
Yordanos A. <107444432+Yordiemma@users.noreply.github.com>
2026-02-02 23:16:51 +0100

    Add server and test http file

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
-require("dotenv").config();
+const express = require('express');
+const app = express();
 
-const connectDB = require("./db");
 
+require('dotenv').config();
+
+
+const connectDB = require('./db');
 connectDB();
 
-console.log("App is running");
 
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.status(200).send('Voice for Her API is running');
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
